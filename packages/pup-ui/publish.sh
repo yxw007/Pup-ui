965eeace73f57dc5c17dbfc78a589e4aa1cf0b37
Potter <75838259@qq.com>
2022-10-11 22:02:57 +0800

    chore: update publish.sh

--- packages/pup-ui/publish.sh
+++ packages/pup-ui/publish.sh
@@ -5,6 +5,6 @@ echo '请进行登录相关操作：'
 npm login # 登陆
 echo "-------publishing-------"
 npm publish # 发布
-# npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
+npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
 echo "发布完成"
 exit
